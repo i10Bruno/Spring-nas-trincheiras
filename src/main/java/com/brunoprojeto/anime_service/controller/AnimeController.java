34548a025b5c2d21f7a414218f65b0d76b7a32a9
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-08-08 11:05:49 -0300

    feat: controller created

---
+++ src/main/java/com/brunoprojeto/anime_service/controller/AnimeController.java
@@ -0,0 +1,13 @@
+package com.brunoprojeto.anime_service.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/animes")
+public class AnimeController {
+
+
+
+}
