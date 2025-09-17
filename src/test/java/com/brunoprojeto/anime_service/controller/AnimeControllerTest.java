03ebb68d73634412f720bfc9950a467b957a58f4
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-09-17 11:11:00 -0300

    Configurando a classe animeControllerTest

--- src/test/java/com/brunoprojeto/anime_service/controller/AnimeControllerTest.java
+++ src/test/java/com/brunoprojeto/anime_service/controller/AnimeControllerTest.java
@@ -1,7 +1,57 @@
 package com.brunoprojeto.anime_service.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.brunoprojeto.anime_service.domain.Anime;
+import com.brunoprojeto.anime_service.mapper.AnimeMapperImpl;
+import com.brunoprojeto.anime_service.mapper.ProducerMapperImpl;
+import com.brunoprojeto.anime_service.repository.AnimeData;
+import com.brunoprojeto.anime_service.repository.AnimeHardCodedRepository;
+import com.brunoprojeto.anime_service.repository.ProducerData;
+import com.brunoprojeto.anime_service.repository.ProducerHardCodedRepository;
+import com.brunoprojeto.anime_service.service.AnimeService;
+import com.brunoprojeto.anime_service.service.ProducerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest(controllers = ProducerController.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Import({AnimeMapperImpl.class , AnimeService.class, AnimeService.class, AnimeData.class})
 class AnimeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockitoSpyBean
+    private AnimeHardCodedRepository repository;
+
+
+    @MockitoBean
+    private AnimeData animeData;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+    private List<Anime> AnimesList;
+
+
+    @BeforeEach
+    void  init () {
+        var ninjaKamui = Anime.builder().id(1L).name("Ninja Kamui").build();
+        var kaijuu = Anime.builder().id(2L).name("Kaijuu-8gou").build();
+        var kimetsuNoYaiba = Anime.builder().id(3L).name("Kimetsu No Yaiba").build();
+
+        AnimesList= new ArrayList<>(List.of(ninjaKamui, kaijuu, kimetsuNoYaiba));
+    }
+
 
 }
