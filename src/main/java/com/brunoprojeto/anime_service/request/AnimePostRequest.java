bb7d9279082821ebf18ef0a137f96bbf7b81b3c1
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-10-11 11:19:36 -0300

    feat:validação nos request de name e id

--- src/main/java/com/brunoprojeto/anime_service/request/AnimePostRequest.java
+++ src/main/java/com/brunoprojeto/anime_service/request/AnimePostRequest.java
@@ -1,11 +1,13 @@
 package com.brunoprojeto.anime_service.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class AnimePostRequest {
+    @NotBlank(message ="The field 'Name' is required")
     private String name;
 
 
